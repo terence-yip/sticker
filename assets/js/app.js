440b9763b66bb8e2d8334c33d9a00dfe3aea44ca
tyip <tsyip88@gmail.com>
2018-03-24 14:38:11 -0700

    Adding robot ui/functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,38 +3,148 @@ var moverightButton = document.getElementById("move-right");
 var moveupButton = document.getElementById("move-up");
 var movedownButton = document.getElementById("move-down");
 
+var lookleftButton = document.getElementById("look-left");
+var lookrightButton = document.getElementById("look-right");
+var lookupButton = document.getElementById("look-up");
+var lookdownButton = document.getElementById("look-down");
+
 function sendMoveCommand(direction, pressed) {
+  sendCommand("move", direction, pressed)
+}
+
+function sendLookCommand(direction, pressed) {
+  sendCommand("look", direction, pressed)
+}
+
+function sendCommand(command, direction, pressed) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("POST", "/api", true);
   xmlHttp.setRequestHeader('Content-Type', 'application/json');
   xmlHttp.send(JSON.stringify({
+    Command: command,
     Direction: direction,
     Pressed: pressed,
   }));
 }
 
 moveleftButton.addEventListener("mousedown", function() {
-  sendMoveCommand("left", "true");
+  sendCommand("move", "left", "true");
 });
 moverightButton.addEventListener("mousedown", function() {
-  sendMoveCommand("right", "true");
+  sendCommand("move", "right", "true");
 });
 moveupButton.addEventListener("mousedown", function() {
-  sendMoveCommand("up", "true");
+  sendCommand("move", "up", "true");
 });
 movedownButton.addEventListener("mousedown", function() {
-  sendMoveCommand("down", "true");
+  sendCommand("move", "down", "true");
 });
 
 moveleftButton.addEventListener("mouseup", function() {
-  sendMoveCommand("left", "false");
+  sendCommand("move", "left", "false");
 });
 moverightButton.addEventListener("mouseup", function() {
-  sendMoveCommand("right", "false");
+  sendCommand("move", "right", "false");
 });
 moveupButton.addEventListener("mouseup", function() {
-  sendMoveCommand("up", "false");
+  sendCommand("move", "up", "false");
 });
 movedownButton.addEventListener("mouseup", function() {
-  sendMoveCommand("down", "false");
+  sendCommand("move", "down", "false");
 });
+
+lookleftButton.addEventListener("mousedown", function() {
+  sendCommand("look", "left", "true");
+});
+lookrightButton.addEventListener("mousedown", function() {
+  sendCommand("look", "right", "true");
+});
+lookupButton.addEventListener("mousedown", function() {
+  sendCommand("look", "up", "true");
+});
+lookdownButton.addEventListener("mousedown", function() {
+  sendCommand("look", "down", "true");
+});
+
+lookleftButton.addEventListener("mouseup", function() {
+  sendCommand("look", "left", "false");
+});
+lookrightButton.addEventListener("mouseup", function() {
+  sendCommand("look", "right", "false");
+});
+lookupButton.addEventListener("mouseup", function() {
+  sendCommand("look", "up", "false");
+});
+lookdownButton.addEventListener("mouseup", function() {
+  sendCommand("look", "down", "false");
+});
+
+moveleftButton.addEventListener("touchstart", function() {
+  sendCommand("move", "left", "true");
+});
+moverightButton.addEventListener("touchstart", function() {
+  sendCommand("move", "right", "true");
+});
+moveupButton.addEventListener("touchstart", function() {
+  sendCommand("move", "up", "true");
+});
+movedownButton.addEventListener("touchstart", function() {
+  sendCommand("move", "down", "true");
+});
+
+moveleftButton.addEventListener("touchend", function() {
+  sendCommand("move", "left", "false");
+});
+moverightButton.addEventListener("touchend", function() {
+  sendCommand("move", "right", "false");
+});
+moveupButton.addEventListener("touchend", function() {
+  sendCommand("move", "up", "false");
+});
+movedownButton.addEventListener("touchend", function() {
+  sendCommand("move", "down", "false");
+});
+
+lookleftButton.addEventListener("touchstart", function() {
+  sendCommand("look", "left", "true");
+});
+lookrightButton.addEventListener("touchstart", function() {
+  sendCommand("look", "right", "true");
+});
+lookupButton.addEventListener("touchstart", function() {
+  sendCommand("look", "up", "true");
+});
+lookdownButton.addEventListener("touchstart", function() {
+  sendCommand("look", "down", "true");
+});
+
+lookleftButton.addEventListener("touchend", function() {
+  sendCommand("look", "left", "false");
+});
+lookrightButton.addEventListener("touchend", function() {
+  sendCommand("look", "right", "false");
+});
+lookupButton.addEventListener("touchend", function() {
+  sendCommand("look", "up", "false");
+});
+lookdownButton.addEventListener("touchend", function() {
+  sendCommand("look", "down", "false");
+});
+
+var timeoutPeriod = 500;
+var imageURI = '/image.jpg';
+var img = new Image();
+img.onload = function() {
+    var canvas = document.getElementById("current-frame");
+    var context = canvas.getContext("2d");
+
+    context.drawImage(img, 0, 0);
+    setTimeout(timedRefresh,timeoutPeriod);
+};
+
+function timedRefresh() {
+    // just change src attribute, will always trigger the onload callback
+    img.src = imageURI + '?d=' + Date.now();
+}
+
+timedRefresh();
--- robit.go
+++ robit.go
@@ -2,26 +2,164 @@ package main
 
 import (
         "fmt"
+        "math"
+        "time"
+        "os/exec"
         "net/http"
         "encoding/json"
-        "gobot.io/x/gobot/drivers/gpio"
         "gobot.io/x/gobot/platforms/raspi"
+        "gobot.io/x/gobot/drivers/i2c"
 )
 
+type ServoType int
+type ServoMode int
+
 type Message struct {
+        Command string
         Direction string
         Pressed string
 }
 
-func update_robot(m Message, led *gpio.LedDriver) {
-        if m.Pressed == "true" {
-                led.On()
-        } else if m.Pressed == "false" {
-                led.Off()
+type Robot struct {
+        motorServoDriver* i2c.AdafruitMotorHatDriver
+        servoModes map[ServoType]ServoMode
+        servoAngle map[ServoType]float64
+}
+
+const (
+        LEFT_MOTOR = 0
+        RIGHT_MOTOR = 1
+
+        SERVO_LOWER_ANGLE_BOUND = -90.0
+        SERVO_UPPER_ANGLE_BOUND = 90.0
+        SERVO_ANGLE_RANGE = SERVO_UPPER_ANGLE_BOUND - SERVO_LOWER_ANGLE_BOUND
+        SERVO_LOWER_ANGLE_PULSE_WIDTH_MS = 1.0
+        SERVO_UPPER_ANGLE_PULSE_WIDTH_MS = 2.0
+        SERVO_PULSE_WIDTH_RANGE = SERVO_UPPER_ANGLE_PULSE_WIDTH_MS - SERVO_LOWER_ANGLE_PULSE_WIDTH_MS
+
+        SERVO_INCREMENT = 1.0
+
+        UPPER_PERIOD_MS = SERVO_UPPER_ANGLE_PULSE_WIDTH_MS + 5
+        SERVO_FREQUENCY_HZ = 1.0 / (UPPER_PERIOD_MS / 1000.0)
+        PERIOD_PER_COUNT_MS = UPPER_PERIOD_MS / 4096
+        LOWER_BOUND_COUNT = SERVO_LOWER_ANGLE_PULSE_WIDTH_MS / PERIOD_PER_COUNT_MS
+)
+
+const (
+        Azimuth ServoType = 8
+        Elevation ServoType = 4
+)
+
+const (
+        Release ServoMode = iota
+        Increase
+        Decrease
+)
+
+func update_robot(m Message, robot *Robot) {
+        switch m.Command {
+        case "move":
+                update_motor(m, robot)
+        case "look":
+                update_servo_mode(m, robot)
         }
 }
 
-func api(led *gpio.LedDriver) func(w http.ResponseWriter, r *http.Request) {
+func update_motor(m Message, robot *Robot) {
+        if m.Pressed == "true" {
+                switch m.Direction {
+                case "left":
+                        robot.motorServoDriver.RunDCMotor(LEFT_MOTOR, i2c.AdafruitBackward)
+                        robot.motorServoDriver.RunDCMotor(RIGHT_MOTOR, i2c.AdafruitForward)
+                case "right":
+                        robot.motorServoDriver.RunDCMotor(LEFT_MOTOR, i2c.AdafruitForward)
+                        robot.motorServoDriver.RunDCMotor(RIGHT_MOTOR, i2c.AdafruitBackward)
+                case "up":
+                        robot.motorServoDriver.RunDCMotor(LEFT_MOTOR, i2c.AdafruitForward)
+                        robot.motorServoDriver.RunDCMotor(RIGHT_MOTOR, i2c.AdafruitForward)
+                case "down":
+                        robot.motorServoDriver.RunDCMotor(LEFT_MOTOR, i2c.AdafruitBackward)
+                        robot.motorServoDriver.RunDCMotor(RIGHT_MOTOR, i2c.AdafruitBackward)
+                default:
+                        robot.motorServoDriver.RunDCMotor(LEFT_MOTOR, i2c.AdafruitRelease)
+                        robot.motorServoDriver.RunDCMotor(RIGHT_MOTOR, i2c.AdafruitRelease)
+                }
+        } else if m.Pressed == "false" {
+                robot.motorServoDriver.RunDCMotor(LEFT_MOTOR, i2c.AdafruitRelease)
+                robot.motorServoDriver.RunDCMotor(RIGHT_MOTOR, i2c.AdafruitRelease)
+        }
+}
+
+func update_servo_mode(m Message, robot *Robot) {
+        if m.Pressed == "true" {
+                switch m.Direction {
+                        case "left":
+                                set_servo(robot, Azimuth, Increase)
+                        case "right":
+                                set_servo(robot, Azimuth, Decrease)
+                        case "up":
+                                set_servo(robot, Elevation, Increase)
+                        case "down":
+                                set_servo(robot, Elevation, Decrease)
+                        default:
+                                set_servo(robot, Azimuth, Release)
+                                set_servo(robot, Elevation, Release)
+                }
+        } else if m.Pressed == "false" {
+                set_servo(robot, Azimuth, Release)
+                set_servo(robot, Elevation, Release)
+        }
+}
+
+func increase_servo(robot *Robot, sType ServoType){
+        robot.servoAngle[sType] = math.Min(
+                robot.servoAngle[sType] + SERVO_INCREMENT, SERVO_UPPER_ANGLE_BOUND)
+}
+
+func decrease_servo(robot *Robot, sType ServoType) {
+        robot.servoAngle[sType] = math.Max(
+                robot.servoAngle[sType] - SERVO_INCREMENT, SERVO_LOWER_ANGLE_BOUND)
+}
+
+func set_servo(robot *Robot, stype ServoType, smode ServoMode) {
+        robot.servoModes[stype] = smode
+}
+
+func update_servo(robot *Robot) {
+        for {
+                switch(robot.servoModes[Azimuth]) {
+                case Increase:
+                        increase_servo(robot, Azimuth)
+                case Decrease:
+                        decrease_servo(robot, Azimuth)
+                }
+
+                switch(robot.servoModes[Elevation]) {
+                case Increase:
+                        decrease_servo(robot, Elevation)
+                case Decrease:
+                        increase_servo(robot, Elevation)
+                }
+                update_servo_angles(robot)
+                time.Sleep(20 * time.Millisecond)
+        }
+}
+
+func update_servo_angles(robot *Robot) {
+        azimuthCounts := angle_to_counts(robot.servoAngle[Azimuth])
+        robot.motorServoDriver.SetServoMotorPulse(byte(Azimuth), 0, azimuthCounts)
+        elevationCounts := angle_to_counts(robot.servoAngle[Elevation])
+        robot.motorServoDriver.SetServoMotorPulse(byte(Elevation), 0, elevationCounts)
+}
+
+func angle_to_counts(angle float64) (int32) {
+        angleRatio := (angle - SERVO_LOWER_ANGLE_BOUND)/SERVO_ANGLE_RANGE
+        period_ms := SERVO_PULSE_WIDTH_RANGE * angleRatio + SERVO_LOWER_ANGLE_PULSE_WIDTH_MS
+        counts := period_ms / PERIOD_PER_COUNT_MS
+        return int32(math.Floor(counts))
+}
+
+func api(robot *Robot) func(w http.ResponseWriter, r *http.Request) {
         return func(w http.ResponseWriter, r *http.Request) {
                 dec := json.NewDecoder(r.Body)
                 var m Message
@@ -29,20 +167,46 @@ func api(led *gpio.LedDriver) func(w http.ResponseWriter, r *http.Request) {
                 if err != nil {
                         fmt.Printf("Error: %v\n", err)
                 } else {
-                        fmt.Printf("Received: %s, %s\n", m.Direction, m.Pressed)
-                        update_robot(m, led)       
+                        update_robot(m, robot)       
                 }
+        }
+}
+
+func start_image_capture() {
+        cmd := exec.Command("raspistill", "-o", "assets/html/image.jpg", "-w", "400", "-h", "300", "-tl", "500", "-t", "0")
+        err := cmd.Start()
+        if err != nil {
+                fmt.Printf("Error:  %v", err)
         }
 }
 
 func main() {
         r := raspi.NewAdaptor()
-        led := gpio.NewLedDriver(r, "36")
+        robot := Robot{
+                i2c.NewAdafruitMotorHatDriver(r),
+                map[ServoType]ServoMode{
+                        Azimuth : Release,
+                        Elevation : Release,
+                },
+                map[ServoType]float64{
+                        Azimuth : 0.0,
+                        Elevation : 0.0,
+                },
+        }
+
+        robot.motorServoDriver.SetMotorHatAddress(0x60)
+        robot.motorServoDriver.Start()
+        robot.motorServoDriver.SetDCMotorSpeed(LEFT_MOTOR, 100)
+        robot.motorServoDriver.SetDCMotorSpeed(RIGHT_MOTOR, 100)
+        robot.motorServoDriver.SetServoHatAddress(0x40)
+        robot.motorServoDriver.SetServoMotorFreq(SERVO_FREQUENCY_HZ)
+        go update_servo(&robot)
+        start_image_capture()
 
         http.Handle("/", http.FileServer(http.Dir("assets/html/")))
         http.Handle("/css/", http.FileServer(http.Dir("assets/")))
         http.Handle("/js/", http.FileServer(http.Dir("assets/")))
-        http.HandleFunc("/api", api(led))
+        http.HandleFunc("/api", api(&robot))
         err := http.ListenAndServe(":8080", nil)
         if err != nil {
             fmt.Printf("ListenAndServe: %v", err)
